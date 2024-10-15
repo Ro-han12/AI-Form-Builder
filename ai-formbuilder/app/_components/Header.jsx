13d59621932ae272c13dec1c60978e2248d3e843
rohan <rohannair2939@gmail.com>
2024-10-15 16:07:58 +0530

    clerk auth done

--- ai-formbuilder/app/_components/Header.jsx
+++ ai-formbuilder/app/_components/Header.jsx
@@ -1,20 +1,29 @@
+"use client"
 import { Button } from '@/components/ui/button'
+import { UserButton, useUser } from '@clerk/nextjs'
 import React from 'react'
 
 function Header() {
-  return (
-    <div className='p-5 border-b shadow-sm'>
-      <div className='flex items-center justify-between'>
-        <div className='text-left'>
-          {/* Other header content (e.g., logo, title, etc.) can go here */}
-          <h1 className='font-bold'>AI FORM BUILDER</h1>
+    const { user, isSignedIn } = useUser();
+    return (
+        <div className='p-5 border-b shadow-sm'>
+            <div className='flex items-center justify-between'>
+                <div className='text-left'>
+                    {/* Other header content (e.g., logo, title, etc.) can go here */}
+                    <h1 className='font-bold'>AI FORM BUILDER</h1>
+                </div>
+                {isSignedIn ? (
+                    <div className='flex items-center gap-5'>
+                        <Button variant="outline">Dashboard</Button>
+                        <UserButton />
+                        <div className='text-right'>
+                            <Button>Get Started</Button>
+                        </div>
+                    </div>
+                ) : null}
+            </div>
         </div>
-        <div className='text-right'>
-          <Button>Get Started</Button>
-        </div>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default Header
+export default Header;
--- ai-formbuilder/app/layout.js
+++ ai-formbuilder/app/layout.js
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./_components/Header";
-
+import {ClerkProvider} from "@clerk/nextjs"
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -20,6 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -28,5 +29,6 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
