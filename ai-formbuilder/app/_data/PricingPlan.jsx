78b8a1ec3990fc8cf949878be58e2c1085c0dbdf
rohan <rohannair2939@gmail.com>
2024-10-17 14:58:23 +0530

    payment feature

---
+++ ai-formbuilder/app/_data/PricingPlan.jsx
@@ -0,0 +1,14 @@
+export default[
+    // {
+    //     link:'https://buy.stripe.com/test_28o9EjbkvgfQ8VOeUV',
+    //     price:4.99,
+    //     priceId:'price_1PIepVCIigsbVqJh5p450LFa',
+    //     duration:'Monthly'
+    // },
+    {
+        link:'https://buy.stripe.com/test_9AQeVJakH6Ee8Lu3ce',
+        price:4.00,
+        priceId:'price_1QAppk01CjhEv6RyPf6FzsXa',
+        duration:'Yearly'
+    }
+]
